f7b14cc3fa11e68b3875a488e6de03f0da5e9040
Isaac <Isaac@192.168.2.236>
2020-03-24 20:20:06 -0400

    second commit

--- src/showwhatyouknow/ShowWhatYouKnow.java
+++ src/showwhatyouknow/ShowWhatYouKnow.java
@@ -8,6 +8,7 @@ package showwhatyouknow;
 /**
  *
  * @author Isaac
+ *  This class is teaching me bout arrays and how to use them to make repeated code quicker.
  */
 import java.util.Scanner;
 public class ShowWhatYouKnow {
@@ -17,27 +18,36 @@ public class ShowWhatYouKnow {
      */
     public static void main(String[] args) {
         Scanner keyedInput = new Scanner (System.in);
-       
+        
+            //creates array:
         double [ ] marks = new double [5];
-      
+        
+         //declares variables:
         double total = 0;
         double average;
-       
+        
+         //asks user for their marks:
         System.out.println("Please enter your last five marks:");
+        
+        //loop that stores five double values for marks:
          for (int i = 0; i <= 4; i = i + 1)
         {
             marks[i] = keyedInput.nextDouble();
         }
-      
+         
+      //Loop that adds the double values together:
          for (int i = 0; i <= 4; i = i + 1)
         {
             total = total + marks[i];
         }
        
+         //uses sum of marks to find average and rounds to one decimal place:
         average = total/5;
         average = average * 10;
         average = Math.round(average);
         average = average/10;
+        
+        //displays mark average to user:
         System.out.println("The average mark is:");
         System.out.println(average);
     }
